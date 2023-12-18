e9facc63ec9dfd62893078948ac5841a3ef51a19
abdullhakim.sami <abdullhakim.sami@gmail.com>
2023-12-18 21:40:21 +0300

    added site and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 
-    // site: 'https://abdullhakim-sami.github.io',
-    // base: '/Sanabil_Al_Firdaws_Astro',
+    site: 'https://abdullhakim-sami.github.io',
+    base: '/Sanabil_Al_Firdaws_Astro',
 });
